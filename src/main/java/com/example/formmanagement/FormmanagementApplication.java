99bac704bcad1bb708afc6941bfd3a3e8e34403a
Vin Com <0plmzxc@gmail.com>
2026-04-30 00:04:01 +0700

    init: setup project structure

---
+++ src/main/java/com/example/formmanagement/FormmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.formmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/formmanagement/FormmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.formmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
